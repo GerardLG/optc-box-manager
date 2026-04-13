e22ea967dd8b785c3dbe2bf96084e3ea955cbd4b
GerardLG <70599227+GerardLG@users.noreply.github.com>
2026-04-13 20:05:46 +0200

    fix: add vite-env.d.ts and extend UnitDetail/ExtendedUnit types

--- src/models/units.ts
+++ src/models/units.ts
@@ -8,14 +8,24 @@ export interface Potential {
 
 export interface UnitDetail {
   captain?: string
+  captainNotes?: string
   special?: string
   specialName?: string
   sailor?: string | { base?: string; level1?: string; level5?: string }
+  sailorNotes?: string
   superSpecial?: string
+  superSpecialCriteria?: string
+  swap?: string | { base?: string; level1?: string; level5?: string }
   potential?: Potential[]
   limit?: string[]
   VSCondition?: string
-  VSSpecial?: string
+  VSSpecial?: unknown
+  VSCaptain?: unknown
+  support?: unknown
+  rumble?: unknown
+  festAbility?: unknown
+  festSpecial?: unknown
+  festResistance?: unknown
 }
 
 export interface ExtendedUnit {
@@ -31,16 +41,20 @@ export interface ExtendedUnit {
   minHP: number
   maxHP: number
   limitHP: number
+  limitexHP: number
   minATK: number
   maxATK: number
   limitATK: number
+  limitexATK: number
   minRCV: number
   maxRCV: number
   limitRCV: number
+  limitexRCV: number
   cooldown?: [number, number]
   aliases?: string[]
   flags?: { global?: boolean; japan?: boolean; rr?: boolean; tn?: boolean }
   detail: UnitDetail
+  evo?: unknown
   evolution?: {
     evolution: number | number[]
     evolvers: (number | 'skullA' | 'skullB' | 'skullC' | 'skullD')[]
